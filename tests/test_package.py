609920b1eeaf2695192971ab6dcef0ea7524cedc
Pablo Prietz <pablo@prietz.org>
2020-11-23 17:43:36 +0100

    Fix test_import()

--- tests/test_package.py
+++ tests/test_package.py
@@ -2,6 +2,8 @@ import pytest
 
 
 def test_import():
+    from pye3d.camera import CameraModel
     from pye3d.detector_3d import Detector3D
 
-    d = Detector3D()
+    camera = CameraModel(1.0, (200, 200))
+    d = Detector3D(camera)
