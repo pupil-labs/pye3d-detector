0b06df0f4f4d63e9f7b8565510ffac0206e2410e
Pablo Prietz <pablo@prietz.org>
2021-06-17 17:39:51 +0200

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -75,5 +75,5 @@ setup(
     package_data={package: ["refraction_models/*.save"]},
     packages=find_packages(),
     url="https://github.com/pupil-labs/pye3d-detector/",
-    version="0.0.7",
+    version="0.0.8",
 )
