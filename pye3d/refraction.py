323daa3006c838eee06126bb3d0f98f7781a3219
Pablo Prietz <pablo@prietz.org>
2021-11-09 19:03:09 +0100

    Change refraction model serialization from pickle to msgpack
    The previous serialization using pickle caused incompatibilities
    when using newer versions of scikit-learn. Since sklearn was not used
    by default anyway, this PR
    - adds a script to convert legacy models to msgpack (only parameters)
    - loads parameters from msgpack files
    - passes parameters directly to cpp pipeline
    - removes scikit-learn + scipy dependencies from default installation
    - removes prior *.save files and replaces them with their *.msgpack
      counterparts

--- pye3d/refraction.py
+++ pye3d/refraction.py
@@ -1,90 +1,95 @@
-import abc
-import os
+import itertools
 from pathlib import Path
 from .cpp.refraction_correction import apply_correction_pipeline
-import numpy as np
 
-import joblib
+import numpy as np
+import msgpack
 
 LOAD_DIR = Path(__file__).parent / "refraction_models"
+LOAD_VERSION = 1
 
 
-def pipeline_to_list(pipeline):
-    return [
-        pipeline[0].powers_.T.astype(float),
-        pipeline[1].mean_[np.newaxis, :].T,
-        pipeline[1].var_[np.newaxis, :].T,
-        pipeline[2].coef_.T,
-        pipeline[2].intercept_[:, np.newaxis].T,
-    ]
+class ModelDeserializationError(Exception):
+    pass
 
 
 class Refractionizer(object):
     def __init__(self, degree=3, type_="default"):
-        self.pipeline_radius = joblib.load(
-            os.path.join(
-                LOAD_DIR, f"{type_}_refraction_model_radius_degree_{degree}.save"
-            )
+        self.pipeline_radius_as_list = Refractionizer.load_config_from_msgpack(
+            "radius", type_, degree
         )
 
-        self.pipeline_gaze_vector = joblib.load(
-            os.path.join(
-                LOAD_DIR, f"{type_}_refraction_model_gaze_vector_degree_{degree}.save"
-            )
+        self.pipeline_gaze_vector_as_list = Refractionizer.load_config_from_msgpack(
+            "gaze_vector", type_, degree
         )
 
-        self.pipeline_sphere_center = joblib.load(
-            os.path.join(
-                LOAD_DIR, f"{type_}_refraction_model_sphere_center_degree_{degree}.save"
-            )
+        self.pipeline_sphere_center_as_list = Refractionizer.load_config_from_msgpack(
+            "sphere_center", type_, degree
         )
 
-        self.pipeline_pupil_circle = joblib.load(
-            os.path.join(
-                LOAD_DIR, f"{type_}_refraction_model_pupil_circle_degree_{degree}.save"
-            )
+        self.pipeline_pupil_circle_as_list = Refractionizer.load_config_from_msgpack(
+            "pupil_circle", type_, degree
         )
 
-        self.pipeline_radius_as_list = pipeline_to_list(self.pipeline_radius)
-        self.pipeline_gaze_vector_as_list = pipeline_to_list(self.pipeline_gaze_vector)
-        self.pipeline_sphere_center_as_list = pipeline_to_list(
-            self.pipeline_sphere_center
-        )
-        self.pipeline_pupil_circle_as_list = pipeline_to_list(
-            self.pipeline_pupil_circle
-        )
+    @staticmethod
+    def load_config_from_msgpack(feature, type_, degree):
+        name = f"{type_}_refraction_model_{feature}_degree_{degree}.msgpack"
+        path = LOAD_DIR / name
+        with path.open("rb") as file:
+            config_model = msgpack.unpack(file)
+            Refractionizer._validate_loaded_model_config(config_model)
+            try:
+                return list(
+                    itertools.chain(
+                        Refractionizer._polynomial_features_from_config(config_model),
+                        Refractionizer._standard_scaler_from_config(config_model),
+                        Refractionizer._linear_regression_from_config(config_model),
+                    )
+                )
+            except KeyError as err:
+                raise ModelDeserializationError from err
+
+    @staticmethod
+    def _validate_loaded_model_config(config_model):
+        if not isinstance(config_model, dict) or "version" not in config_model:
+            raise ModelDeserializationError("Unrecognized format")
+        if config_model["version"] != LOAD_VERSION:
+            raise ModelDeserializationError(
+                f"Unexpected version `{config_model['version']}` "
+                f"(expected `{LOAD_VERSION}``)"
+            )
+
+    @staticmethod
+    def _polynomial_features_from_config(config_model):
+        yield np.array(config_model["steps"]["PolynomialFeatures"]["powers"])
+
+    @staticmethod
+    def _standard_scaler_from_config(config_model):
+        config_scaler = config_model["steps"]["StandardScaler"]
+        yield np.array(config_scaler["mean"])
+        yield np.array(config_scaler["var"])
+
+    @staticmethod
+    def _linear_regression_from_config(config_model):
+        config_lin_reg = config_model["steps"]["LinearRegression"]
+        yield np.array(config_lin_reg["coef"])
+        yield np.array(config_lin_reg["intercept"])
 
     @staticmethod
     def _apply_correction_pipeline(X, pipeline_arrays):
         return apply_correction_pipeline(np.asarray(X).T, *pipeline_arrays)
 
-    def correct_radius(self, X, implementation="cpp"):
-        if implementation == "cpp":
-            y = self._apply_correction_pipeline(X, self.pipeline_radius_as_list)
-        else:
-            y = self.pipeline_radius.predict(X)
-        return y
+    def correct_radius(self, X):
+        return self._apply_correction_pipeline(X, self.pipeline_radius_as_list)
 
-    def correct_gaze_vector(self, X, implementation="cpp"):
-        if implementation == "cpp":
-            y = self._apply_correction_pipeline(X, self.pipeline_gaze_vector_as_list)
-        else:
-            y = self.pipeline_gaze_vector.predict(X)
-        return y
+    def correct_gaze_vector(self, X):
+        return self._apply_correction_pipeline(X, self.pipeline_gaze_vector_as_list)
 
-    def correct_sphere_center(self, X, implementation="cpp"):
-        if implementation == "cpp":
-            y = self._apply_correction_pipeline(X, self.pipeline_sphere_center_as_list)
-        else:
-            y = self.pipeline_sphere_center.predict(X)
-        return y
+    def correct_sphere_center(self, X):
+        return self._apply_correction_pipeline(X, self.pipeline_sphere_center_as_list)
 
-    def correct_pupil_circle(self, X, implementation="cpp"):
-        if implementation == "cpp":
-            y = self._apply_correction_pipeline(X, self.pipeline_pupil_circle_as_list)
-        else:
-            y = self.pipeline_pupil_circle.predict(X)
-        return y
+    def correct_pupil_circle(self, X):
+        return self._apply_correction_pipeline(X, self.pipeline_pupil_circle_as_list)
 
 
 if __name__ == "__main__":
