53d0eb8b93d24cb4f76cb2c69571c39d2add07aa
Pablo Prietz <pablo@prietz.org>
2022-07-04 15:52:26 +0200

    Consistency

--- pye3d/observation.py
+++ pye3d/observation.py
@@ -1,13 +1,3 @@
-"""
-(*)~---------------------------------------------------------------------------
-Pupil - eye tracking platform
-Copyright (C) 2012-2019 Pupil Labs
-
-Distributed under the terms of the GNU
-Lesser General Public License (LGPL v3.0).
-See COPYING and COPYING.LESSER for license details.
----------------------------------------------------------------------------~(*)
-"""
 from abc import abstractmethod, abstractproperty
 from collections import deque
 from math import floor
